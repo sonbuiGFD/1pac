31ff847135fb732ade9bfa7ff9566a396048762b
sonbui <sonbui.gfd@gmail.com>
2020-04-07 13:42:08 +0700

    update footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './style.scss';
+
+export const Footer = () => (
+  <footer className="footer__main">
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          <p className="text-left">© 2020 son_bui_</p>
+        </div>
+        <div className="col-6">
+          <p className="text-right">sonbui.gfd@gmail.com</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+);
+
+export default Footer;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+
 import Header from 'components/header';
+import Footer from 'components/footer';
 
 const Dashboard = () => (
   <div className="onepac">
     <Header />
+    <main>
+      <div className="container">this is home</div>
+    </main>
+    <Footer />
   </div>
 );
 
